9c7754202bb9ffe76d725504ad32dceb7f24d752
Raycho Mukelov <raycho.mukelov@opencode.com>
2016-08-03 15:08:18 +0300

    - shano stuff

---
+++ test_123/mutate.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+template <class Pair>
+struct Reverse
+{
+  typedef typename Pair::first_type second_type;
+  typedef typename Pair::second_type first_type;
+
+  second_type second;
+  first_type first;
+};
+
+template <class Pair>
+Reverse<Pair> & mutate(Pair & p)
+{
+  return reinterpret_cast<Reverse<Pair> &>(p);
+}
+
+int main(void)
+{
+  pair<double, int> p(1.34, 5);
+  cout << "p.first = " << p.first << ", p.second = " << p.second << endl;
+  cout << "mutate(p).first = " << mutate(p).first << ", mutate(p).second = " << mutate(p).second << endl;
+}
---
+++ test_123/shano.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class MakeFinal
+{
+  ~MakeFinal() {} // private by default.
+  friend class sealed;
+};
+
+
+class sealed : virtual MakeFinal
+{ };
+
+class test : public sealed
+{ };
+
+template <int N> struct Fib {
+    enum {
+        value = Fib<N - 1>::value + Fib<N - 2>::value
+    };
+};
+
+template <> struct Fib<1> {
+    enum { value = 1 };
+};
+
+template <> struct Fib<2> {
+    enum { value = 1 };
+};
+
+enum ENUMTEST
+{
+  ZERO 	= 0,
+  ONE 	= 1,
+  TWO 	= 2
+};
+
+ENUMTEST a[]={ONE, TWO, ZERO};
+
+class VisualizableVector 
+{
+  public:
+    class proxy 
+    {
+      public:
+      proxy ( vector<int>* v ) : vect ( v )
+      {
+	std::cout << "Before size is: " << vect->size ();
+      }
+      vector<int> * operator->()
+      {
+	return vect;
+      }
+      ~proxy ()
+      {
+	std::cout << "After size is: " << vect->size ();
+      }
+      private:
+	vector <int> * vect;
+    };
+    VisualizableVector( vector<int> *v ) : vect( v ) {}
+    
+    proxy operator->()
+    {
+      return proxy (vect);
+    }
+
+  private:
+    vector<int>* vect;
+};
+
+namespace xxx
+{
+  enum ALA_BALA
+  {
+    a = 1,
+    b = 2,
+    c = 3
+  };
+}
+
+int main()
+{
+  VisualizableVector v( new vector<int> );
+  //...
+  v->push_back( 10 ); // Note use of -> operator instead of . operator
+  v->push_back( 20 );
+}
+
+// int main (void)
+// {
+//   //test t; // Compilation error here.
+// 
+//   int x = Fib<10>::value; // 55
+//   cout << x << endl;
+//   
+//   int y = Fib<14>::value; // 377
+//   cout << y << endl;
+// 
+//   int i = -15;
+//   ENUMTEST eTest = static_cast<ENUMTEST>(i);
+//   
+//   cout << "eTest=" << eTest << endl;
+// 
+//   cout << sizeof(a)/sizeof(ENUMTEST) << endl;
+//   return 0;
+// }
+// 
